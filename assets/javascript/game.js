16d656eb4e78c148df7f329e3a96957603e1bd0b
Greg Murphy <bopgregorymurphy@gmail.com>
2018-09-08 22:53:46 -0700

    message

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+
+//Sets the user aggregate variable to 0 to start game
+var userAggregate = 0;
+
+//Lets the computer select a random letter from 19 to 120
+var compRandom = Math.floor(Math.random() * 102 + 19);
+
+//Create and set variable for each of the crystals when clicked.
+var crystalRandom = Math.floor(Math.random() * 12 + 1);
+
+
+// Function will be called when we reset everything
+var reset = function() {
+                        userAggregate = 0;
+    
+                        }
+
+
